1f40d941d77d0a01e1c413cc350203e3c6f3e54b
JohnDelaPena <johnosrail1998@yahoo.com>
2024-07-03 20:20:52 +0100

    Added stub Methods to Game class.

--- src/griffith/Game.java
+++ src/griffith/Game.java
@@ -1,5 +1,29 @@
 package griffith;
+import java.util.HashSet;
 
 public class Game {
-
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public HashSet<String>generateNumbers() {
+		return null;
+		
+	}
+	
+	public int checkWinner() {
+		return 0;
+		
+	}
+	
+	public double prizeWon() {
+		return 0;
+		
+	}
+	
+	public boolean verifyGuesses() {
+		return false;
+		
+	}
 }
