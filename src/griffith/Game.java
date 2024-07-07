0fa8a2d9963a20745be2f8886056da62ee965fab
JohnDelaPena <johnosrail1998@yahoo.com>
2024-07-07 19:19:45 +0100

    Added a unit test to GameTest.java which tests that the numbers below 10 in the generateNumbers() method have a 0 before it for the sake of consistency.

--- src/griffith/Game.java
+++ src/griffith/Game.java
@@ -1,29 +1,49 @@
 package griffith;
 import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.Scanner;
 
 public class Game {
-	
+
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter 6 Numbers");//Prompt user to input 6 numbers
+		Set<String> inputSet= new HashSet<String>();
+		int count=0; //
+		while(count<6) {
+			int input=scan.nextInt();
+			if(input<10) { //If the  number input is less than 10,
+				inputSet.add(String.valueOf("0"+input));// add 0 before the number for consistency.
+			}
+			else { //Else if above 10,
+				inputSet.add(String.valueOf(input));// just add the number to set as is.
+			}
+			count++;//Increment count by 1 after each input from the user.
+		}
+		System.out.println(inputSet);
 		
 	}
-	
-	public HashSet<String>generateNumbers() {
+
+
+	public HashSet <String> generateNumbers() {
+		
 		return null;
 		
 	}
-	
+
 	public int checkWinner() {
 		return 0;
-		
+
 	}
-	
+
 	public double prizeWon() {
 		return 0;
-		
+
 	}
-	
-	public boolean verifyGuesses() {
+
+	public boolean verifyGuesses(HashSet<String>Guesses) {
 		return false;
-		
+
 	}
 }
--- src/griffith/GameTest.java
+++ src/griffith/GameTest.java
@@ -2,6 +2,9 @@ package griffith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -27,7 +30,32 @@ class GameTest {
 	}
 
 	@Test
-	void test() {
+	void testGenerateNumbers() {
+		Game game = new Game();//Create an instance of game.
+		
+		//Test 1: Test that the numbers below 10 have a "0" beside it for consistency.
+		 Set<String> winningNumbers = game.generateNumbers(); // Let winning numbers to be tested equal to the winning numbers in game class.
+		 
+		 for(String number : winningNumbers) {//Create a loop which traverses the set.
+			 int num = Integer.parseInt(number);//The integer num is the number used to see if the content of the set is below 10.
+				if(num<10) { // Test that the numbers below 10 have a "0" before it for consistency.
+					assertTrue(number.startsWith("0")); //The result of the test is a true or false.
+				}
+		 }
+	}
+
+	@Test
+	void testCheckWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrizeWon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerifyGuesses() {
 		fail("Not yet implemented");
 	}
 
